4c28b4898437cf62780e887da18633bc17d56557
euller <eullerpenaregis@gmail.com>
2025-01-29 17:31:08 -0300

    created the button component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import styles from './page.module.css'
+import {Button} from '../components/Button/Button'
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Ofertify</h1>
+      <Button text="button"> </Button>
+      <Button text="search"> </Button>
     </div>
   )
 }
