97edde535f8e3e98cc00284def3773b5956f7579
euller <eullerpenaregis@gmail.com>
2025-02-07 16:58:29 -0300

    start of the react tutorial

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,6 @@ import styles from './page.module.css'
 import {Button} from '../components/Button/Button'
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <h1>Ofertify</h1>
-      <Button text="button"> </Button>
-      <Button text="search"> </Button>
-    </div>
-  )
+  return <button className="square">X</button>;
+  
 }
