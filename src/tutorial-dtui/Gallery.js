950db0bd8dc31e54a0efe97369456aed4a53912f
euller <eullerpenaregis@gmail.com>
2025-04-02 17:26:41 -0300

    updated wsi

---
+++ src/tutorial-dtui/Gallery.js
@@ -0,0 +1,21 @@
+
+
+function Profile(){
+    return (
+        <ul 
+            style={{
+                backgroundColor: 'black',
+                color: 'pink'
+            }}
+        
+        />
+    );
+}
+
+export default function Gallery(){
+    return(
+        <div>
+            <Profile />
+        </div>
+    )
+}
---
+++ src/tutorial-dtui/page.js
@@ -0,0 +1,10 @@
+import Gallery from './Gallery.js';
+import { Profile } from './Gallery.js';
+import Packing from './Packing.js';
+
+export default function Page() {
+    return(
+        <Packing/>
+
+    );
+}
